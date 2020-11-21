8979a345714e347e9b6df65c16b4a61a841af0e1
Laura-fr <laurafraysse84@gmail.com>
2020-11-21 16:59:32 +0100

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img alt="logo" src="/marvel-logo.png" />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/containers/Characters.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Characters = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://marvel-backend-20.herokuapp.com/characters"
+      );
+      console.log(response.data);
+      setData(response.data);
+
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return isLoading ? (
+    <span>En cours de chargement...</span>
+  ) : (
+    <div className="characterpage">
+      <h2>DISCOVER OUR CHARACTERS</h2>
+      <div className="character">
+        {data.results.map((results, index) => {
+          return (
+            <div className="characterbox" key={index}>
+              <h3>{results.name}</h3>
+              {/* <img
+              alt={results.thumbnail.path}
+              src={(results.thumbnail[keys[0]], results.thumbnail[keys[1]])}
+            /> */}
+              <span>{results.description}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Characters;
